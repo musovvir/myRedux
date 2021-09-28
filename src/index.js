7749e42240ab79ce968d5dcaaf052de299ceff1d
Висхан Музагов <musovvir@MacBook-Air-Vishan.local>
2021-09-28 11:28:07 +0300

    add project

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import {createStore, applyMiddleware, compose} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {rootReducer} from "./redux/rootReducer";
+import logger from 'redux-logger'
+import {asyncIncrement, changeTheme, decrement, increment} from "./redux/actions";
+import thunk from 'redux-thunk'
+import './styles.css'
+
+const  counter = document.getElementById('counter')
+const  addBtn = document.getElementById('add')
+const  subBtn = document.getElementById('sub')
+const  asyncBtn = document.getElementById('async')
+const  themeBtn = document.getElementById('theme')
+
+const store = createStore(
+    rootReducer,
+    compose(
+        composeWithDevTools(applyMiddleware(thunk, logger))
+    )
+)
+
+window.store = store
+
+addBtn.addEventListener('click', () => {
+    store.dispatch(increment())
+})
+
+subBtn.addEventListener('click', () => {
+    store.dispatch(decrement())
+})
+
+asyncBtn.addEventListener('click', () => {
+    store.dispatch(asyncIncrement())
+})
+
+themeBtn.addEventListener('click', () => {
+    const newTheme = document.body.classList.contains('light')
+    ? 'dark'
+    : 'light'
+    store.dispatch(changeTheme(newTheme))
+ })
+
+store.subscribe(() => {
+    const state = store.getState()
+
+    counter.textContent = state.counter
+    document.body.className = state.theme.value;
+
+    [addBtn, subBtn, themeBtn, asyncBtn].forEach(btn => {
+        btn.disabled = state.theme.disabled
+    })
+})
+
+store.dispatch({type: 'INIT_APPLICATION'})
